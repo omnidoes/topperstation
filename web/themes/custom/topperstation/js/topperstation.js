071de76d6b5099aad0b8195c0e592e37f0375031
Aaron Silber <aaron.silber@omniperforms.com>
2019-04-26 16:40:11 -0400

    - created a theme based on Lullabot's windup - configured the header region - added the /shows route - created a view for the shows listing

---
+++ web/themes/custom/topperstation/js/topperstation.js
@@ -0,0 +1,16 @@
+(function ($) {
+
+  'use strict';
+
+  /**
+   * Example function.
+   * @see https://www.drupal.org/node/304258#drupal-behaviors
+   */
+  Drupal.behaviors.sayHello = {
+    'attach': function (context) {
+      // Do JavaScript things in here!
+      // console.log('Hello, world!');
+    }
+  };
+
+})(jQuery);
